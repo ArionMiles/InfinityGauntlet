f83f57b9172789b79b9bdcb3704502d04fc81b75
Ujjwal Gupta <ugupta41@gmail.com>
2018-07-02 19:55:00 +0530

    Implemented a recursive function to retrieve all comments and replies from a given thread

--- scraper.py
+++ scraper.py
@@ -3,6 +3,9 @@ import praw
 from settings import *
 from database import init_db, connection
 from models import Users
+from praw.models.reddit.comment import Comment
+from praw.models.reddit.more import MoreComments
+from praw.models.comment_forest import CommentForest
 
 
 def authenticate(user_agent, app_key, app_secret, username, password):
@@ -26,13 +29,8 @@ def scrape(reddit):
     post_number = 1
     for submission in reddit.subreddit(subreddit).top(limit=SEARCH_LIMIT):
         print(f"Scraping contributors from thread (including all comments) number: {post_number}")
-        all_comments = submission.comments.list()  # Get all nested comments as well, in a flattened list
-        authors = [submission.author.name]
-        for comment in all_comments:
-            try:
-                authors.append(comment.author.name)
-            except AttributeError:
-                continue
+        authors = get_all_authors_from_submission(submission)
+        authors.append(submission.author.name)
         unique_authors = list(set(authors))
         data = [{'username': unique_author, 'saved': False} for unique_author in unique_authors]
         query = Users.__table__.insert(prefixes=["OR REPLACE"])
@@ -40,7 +38,36 @@ def scrape(reddit):
         post_number += 1
     connection.close()
 
+
+total_authors = 0
+
+
+def get_all_authors_from_submission(submission):
+    all_parent_comments = submission.comments.list()  # Get all nested comments as well, in a flattened list
+    global total_authors
+    total_authors = 0
+    all_comments = get_all_nested_replies_from_comment_recursively(all_parent_comments)
+    return all_comments
+
+
+def get_all_nested_replies_from_comment_recursively(comments):
+    all_comments = []
+    for comment in comments:
+        if isinstance(comment, Comment):
+            if comment and comment.author and comment.author.name:
+                global total_authors
+                total_authors += 1
+                if total_authors % 100 == 0:
+                    print(f"Total authors extracted from this thread: {total_authors}")
+                all_comments.append(comment.author.name)
+        if isinstance(comment, MoreComments):
+            all_comments.extend(get_all_nested_replies_from_comment_recursively(comment.comments()))
+        if isinstance(comment, CommentForest):
+            all_comments.extend(get_all_nested_replies_from_comment_recursively(comment.list()))
+    return all_comments
+
+
 if __name__ == '__main__':
-    SEARCH_LIMIT = 5
+    SEARCH_LIMIT = 3
     reddit = authenticate(user_agent, app_key, app_secret, username, password)
     scrape(reddit)
